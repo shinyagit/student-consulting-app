92e92e96ba11f14587627e41e54cccd8cf62d134
shinya <sny.0625hks@gmail.com>
2026-04-16 22:39:20 +0900

    Add mPDF font files and configure fontDir

--- app/Http/Controllers/GuidanceRecordPdfController.php
+++ app/Http/Controllers/GuidanceRecordPdfController.php
@@ -11,19 +11,6 @@ class GuidanceRecordPdfController extends Controller
 {
     public function show(GuidanceRecord $guidanceRecord)
     {
-        $guidanceRecord->load(['student', 'user']);
-
-        $this->authorize('view', $guidanceRecord->student);
-
-        $html = view('pdf.guidance-record', [
-            'record' => $guidanceRecord,
-        ])->render();
-
-        $tempDir = storage_path('app/mpdf-temp');
-        if (! is_dir($tempDir)) {
-            mkdir($tempDir, 0775, true);
-        }
-
         $defaultConfig = (new ConfigVariables())->getDefaults();
         $fontDirs = $defaultConfig['fontDir'];
 
@@ -31,17 +18,10 @@ class GuidanceRecordPdfController extends Controller
         $fontData = $defaultFontConfig['fontdata'];
 
         $mpdf = new Mpdf([
-            'mode' => 'utf-8',
-            'format' => 'A4',
-            'tempDir' => $tempDir,
             'fontDir' => array_merge($fontDirs, [
-                storage_path('fonts'),
+                resource_path('fonts'),
             ]),
             'fontdata' => $fontData + [
-                'notosansjp' => [
-                    'R' => 'NotoSansJP-VariableFont_wght.ttf',
-                    'B' => 'NotoSansJP-VariableFont_wght.ttf',
-                ],
                 'mplus1' => [
                     'R' => 'MPLUS1p-Regular.ttf',
                     'B' => 'MPLUS1p-Bold.ttf',
@@ -50,11 +30,19 @@ class GuidanceRecordPdfController extends Controller
             'default_font' => 'mplus1',
         ]);
 
+        $html = view('guidance-records.pdf', [
+            'record' => $guidanceRecord,
+        ])->render();
+
         $mpdf->WriteHTML($html);
 
-        return response($mpdf->Output('', 'S'), 200, [
-            'Content-Type' => 'application/pdf',
-            'Content-Disposition' => 'inline; filename="guidance-record-' . $guidanceRecord->id . '.pdf"',
-        ]);
+        return response(
+            $mpdf->Output('', 'S'),
+            200,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'inline; filename="guidance-record.pdf"',
+            ]
+        );
     }
 }
