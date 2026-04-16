f0394d55b9a6813afb97579b46b7b3633a23fb6b
shinya <sny.0625hks@gmail.com>
2026-04-16 21:00:01 +0900

    Fix public index.php for Xserver path

--- public/index.php
+++ public/index.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = '/home/xs266894/laravel-app/current/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = '/home/xs266894/laravel-consulting-app/current/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require '/home/xs266894/laravel-app/current/vendor/autoload.php';
+require '/home/xs266894/laravel-consulting-app/current/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once '/home/xs266894/laravel-app/current/bootstrap/app.php';
+$app = require_once '/home/xs266894/laravel-consulting-app/current/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
