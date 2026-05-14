13908d4d9e3d77c114f70ad3624c042355f8364b
shinya <sny.0625hks@gmail.com>
2026-05-14 12:12:45 +0900

    全生徒記録出力機能追加

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,26 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\GuidanceRecordController;
 use App\Http\Controllers\GuidanceRecordPdfController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentPdfExportController;
+use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::resource('users', UserController::class)->except(['show', 'destroy']);
-});
-
-Route::middleware(['auth'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
+    Route::resource('users', UserController::class)->except(['show', 'destroy']);
     Route::resource('students', StudentController::class)->except(['destroy']);
     Route::resource('teachers', TeacherController::class)->except(['destroy']);
     Route::resource('guidance-records', GuidanceRecordController::class)
         ->except(['index', 'show', 'destroy']);
+
     Route::get('/guidance-records/{guidanceRecord}/pdf', [GuidanceRecordPdfController::class, 'show'])
         ->name('guidance-records.pdf');
+
+    Route::get('/students/export/all-pdfs', [StudentPdfExportController::class, 'exportAll'])
+        ->name('students.export.all-pdfs');
 });
